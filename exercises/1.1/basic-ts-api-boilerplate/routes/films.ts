ce1cf83572193cfbff235d1e080b59ef498a7901
Raffaele Hulot <raffaele.hulot@student.vinci.be>
2025-09-19 12:02:43 +0200

    new: ex1.1

---
+++ exercises/1.1/basic-ts-api-boilerplate/routes/films.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+
+//import path from "node:path";
+import { Film } from "../types";
+
+const router = Router();
+
+const defaultFilms: Film[] = [
+    {
+        id: 1,
+        title: "memento",
+        director: "Christopher Nolan",
+        duration: 113
+    },
+    {
+        id: 2,
+        title: "Willy's wonderland",
+        director: "Kevin Lewis",
+        duration: 88
+    },
+    {
+        id: 3,
+        title: "alien",
+        director: "Ridley Scott",
+        duration: 117
+    }
+];
+
+router.get("/", (_req, res) => {
+    const films = defaultFilms;
+
+    return res.json(films)
+});
+
+export default router;
