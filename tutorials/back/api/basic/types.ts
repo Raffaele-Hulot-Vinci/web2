6c3b783bdb79110b7e290544a80ee71f2a4489e4
Raffaele Hulot <raffaele.hulot@student.vinci.be>
2025-09-22 15:29:40 +0200

    tutorial update

--- tutorials/back/api/basic/types.ts
+++ tutorials/back/api/basic/types.ts
@@ -9,6 +9,8 @@ interface PizzaToUpdate {
   content?: string;
 }
 
+type NewPizza = Omit<Pizza, "id">;
+
 interface Drink {
   id: number;
   title: string;
@@ -17,6 +19,6 @@ interface Drink {
   price: number;
 }
 
-type NewPizza = Omit<Pizza, "id">;
+type NewDrink = Omit<Drink, "id">;
 
-export type { Pizza, NewPizza, PizzaToUpdate, Drink };
+export type { Pizza, NewPizza, PizzaToUpdate, Drink, NewDrink };
